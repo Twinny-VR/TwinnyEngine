4c1980b6b68dbe6b5b4bf43d930670016428ac7d
Twinny VR <dev@twinnyvr.com.br>
2025-06-25 16:18:16 -0300

    AddAssemblyToWeave implemented on InstallOpenXRPlatform

--- Runtime/OpenXR/InstallOpenXRPlatform.cs
+++ Runtime/OpenXR/InstallOpenXRPlatform.cs
@@ -1,6 +1,9 @@
 #if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using Fusion;
 using NUnit.Framework;
 using Twinny.System;
 using UnityEditor;
@@ -12,12 +15,14 @@ namespace Twinny.XR
     [InitializeOnLoad]
     public static class InstallOpenXRPlatform
     {
-        private const string Symbol = "TWINNY_OPENXR";
+        private const string OPENXR_SYMBOL = "TWINNY_OPENXR";
+        private const string FUSION_NETWORK_CONFIG_PATH = "Assets/Photon/Fusion/Resources/NetworkProjectConfig.fusion";
 
         static InstallOpenXRPlatform()
         {
             DefinePlatform();
             CheckForSamples();
+            AddAssemblyToWeave(typeof(InstallOpenXRPlatform));
         }
 
         private static void DefinePlatform()
@@ -25,18 +30,43 @@ namespace Twinny.XR
             NamedBuildTarget buildTarget = NamedBuildTarget.Android;
             string defines = PlayerSettings.GetScriptingDefineSymbols(buildTarget);
 
-            if (!defines.Contains(Symbol))
+            if (!defines.Contains(OPENXR_SYMBOL))
             {
-                Debug.LogWarning($"[Twinny] Define symbol '{Symbol}' added to Android build target.");
+                Debug.LogWarning($"[Twinny] Define symbol '{OPENXR_SYMBOL}' added to Android build target.");
 
                 if (string.IsNullOrEmpty(defines))
-                    defines = Symbol;
+                    defines = OPENXR_SYMBOL;
                 else
-                    defines += ";" + Symbol;
+                    defines += ";" + OPENXR_SYMBOL;
 
                 PlayerSettings.SetScriptingDefineSymbols(buildTarget, defines);
             }
         }
+
+        public static void AddAssemblyToWeave(Type type)
+        {
+
+            var assembly = type.Assembly.GetName().Name;
+            var fusionConfig = AssetDatabase.LoadAssetAtPath<NetworkProjectConfigAsset>(FUSION_NETWORK_CONFIG_PATH);
+            if (fusionConfig == null)
+            {
+                Debug.LogError($"[InstallOpenXRPlatform] NetworkProjectConfig not found in '{FUSION_NETWORK_CONFIG_PATH}'");
+                return;
+            }
+
+            var config = fusionConfig.Config;
+            List<string> assemblies = new List<string>(config.AssembliesToWeave);
+
+            if (assemblies.Contains(assembly)) 
+                return;
+
+                assemblies.Add(assembly);
+                config.AssembliesToWeave = assemblies.ToArray();
+                EditorUtility.SetDirty(fusionConfig);
+                AssetDatabase.SaveAssets();
+                Debug.Log($"Assembly '{assembly}' added to Fusion AssembliesToWeave Network Config File.");
+        }
+
 
         private static void CheckForSamples()
         {
@@ -65,7 +95,6 @@ namespace Twinny.XR
                 }
             }
         }
-
         private static void ConfigureScenesInBuild(string scenePath)
         {
             var scenes = new[]
